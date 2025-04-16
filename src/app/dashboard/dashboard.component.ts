bd2ad797518917a37e0b9372785f3d5597f5bd23
Chedy Chaaben <chedychaaben@gmail.com>
2025-04-16 17:49:43 +0100

    working project repair

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,16 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  // Pie chart data
-  public pieChartLabels: string[] = ['Sales', 'Marketing', 'Development'];
-  public pieChartData: number[] = [120, 150, 180];
-  public pieChartType: string = 'pie';
-  public pieChartOptions = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      }
-    }
-  };
 }
